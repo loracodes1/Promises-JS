02c4ca686275b320b80c6276f69c54124c12dca2
florence <loracodes3@gmail.com>
2024-11-05 15:30:09 +0300

    explain promises

---
+++ index.js
@@ -0,0 +1,9 @@
+// Promise = An Object that manages asynchronous operations.
+//                    Wrap a Promise Object around {asynchronous code}
+//                    "I promise to return a value"
+
+// DO THESE CHORES IN ORDER
+
+// 1. WALK THE DOG
+// 2. CLEAN THE KITCHEN
+// 3. TAKE OUT THE TRASH
